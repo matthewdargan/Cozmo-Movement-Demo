69cf1c857d7ef4a33f29da2dc029cbaf32b3c436
Matthew Dargan <matthewdargan57@gmail.com>
2019-01-17 11:55:22 -0600

    Complete Variation 2 code

--- CozmoClassMovement.py
+++ CozmoClassMovement.py
@@ -14,8 +14,8 @@ from cozmo.util import degrees, distance_mm
 
 #import libraries for light colors.  If this library has not been
 #acquired, comment out all actions involving colors
-from colors import Colors
-from woc import WOC
+#from colors import Colors
+#from woc import WOC
 
 #import _thread
 #import time
@@ -23,60 +23,82 @@ from woc import WOC
 def moveInCircle(robot, speed, seconds):
 
 	robot.say_text("I will spin in three circles").wait_for_completed()
-	robot.set_all_backpack_lights(Colors.BLUE)
+	#robot.set_all_backpack_lights(Colors.BLUE)
 	#the first value is the speed for one of the treads, and the second value
 	#is the speed for the other tread (left? right?).  They can both be
 	#the same sign, or have opposite signs.  the last value is the duration of the
 	#movement (measured in seconds?)
 	robot.drive_wheels(speed, -1 * speed, None, None, seconds)
-	robot.set_all_backpack_lights(Colors.GREEN)
+	#robot.set_all_backpack_lights(Colors.GREEN)
 	return
 
 def turnRight(robot):
 	robot.say_text("I will turn right").wait_for_completed()
-	robot.set_all_backpack_lights(Colors.RED)
+	#robot.set_all_backpack_lights(Colors.RED)
 	robot.turn_in_place(cozmo.util.degrees(-90)).wait_for_completed()
-	robot.set_all_backpack_lights(Colors.GREEN)
+	#robot.set_all_backpack_lights(Colors.GREEN)
 	return
 
 def turnLeft(robot):
 	robot.say_text("I will turn left").wait_for_completed()
-	robot.set_all_backpack_lights(Colors.BLUE)
+	#robot.set_all_backpack_lights(Colors.BLUE)
 	robot.turn_in_place(cozmo.util.degrees(90)).wait_for_completed()
-	robot.set_all_backpack_lights(Colors.GREEN)
+	#robot.set_all_backpack_lights(Colors.GREEN)
+	return
+
+def turnReverse(robot):
+	robot.say_text("I will reverse").wait_for_completed()
+	robot.turn_in_place(cozmo.util.degrees(180)).wait_for_completed()
 	return
 
 def cozmo_program(robot: cozmo.robot.Robot):
-	
-	# Move lift down and tilt the head up
-	robot.move_lift(-3)
-	robot.set_head_angle(degrees(0)).wait_for_completed()
-    
-	#make an announcment and drive forward
-	robot.say_text("I will drive forward").wait_for_completed()
-	robot.drive_straight(cozmo.util.distance_mm(400), cozmo.util.speed_mmps(150)).wait_for_completed()
-	
-	#Cozmo should turn left (counter-clockwise)
-	turnLeft(robot)    
-	
-	#make an announcment and drive forward
-	robot.say_text("I will drive forward again").wait_for_completed()
-	robot.drive_straight(cozmo.util.distance_mm(400), cozmo.util.speed_mmps(150)).wait_for_completed()
-
-	#Cozmo should turn right (clockwise)
-	turnRight(robot)
-	
-	#make an announcment and drive forward
-	robot.say_text("I will drive forward one more time").wait_for_completed()
-	robot.drive_straight(cozmo.util.distance_mm(400), cozmo.util.speed_mmps(150)).wait_for_completed()
-	
-	#make an announcment and act like a dog
-	robot.say_text("I will act like a dog").wait_for_completed()
-	robot.play_anim(name="anim_petdetection_dog_03").wait_for_completed()
-	
-	#moveInCircle(robot, speed, seconds)
-	moveInCircle(robot, 200, 6)
-	
+	cozmo_movement(robot, a = 0, b = 0, x = -400, y = 400)	
 	return
 
-cozmo.run_program(cozmo_program, use_viewer=False, force_viewer_on_top=False)
+def cozmo_movement(robot: cozmo.robot.Robot,  x, y, a = 0, b = 0, animation = "anim_petdetection_dog_03"):
+	x_mag = abs(x - a)
+	y_mag = abs(y - b)
+
+	# Just go straight
+	if (x-a >= 0):
+		robot.say_text("I will drive forward one more time").wait_for_completed()
+		robot.drive_straight(cozmo.util.distance_mm(x_mag), cozmo.util.speed_mmps(150)).wait_for_completed()
+	
+	# Reverse
+	else:
+		# Turn 180 degrees
+		turnReverse(robot)
+
+		# Then move straight
+		robot.say_text("I will drive forward one more time").wait_for_completed()
+		robot.drive_straight(cozmo.util.distance_mm(x_mag), cozmo.util.speed_mmps(150)).wait_for_completed()
+
+		# turn back to positive x direction
+		turnReverse(robot)
+	
+
+	if (y-b >= 0):
+		# Turn in the positive y-direction
+		turnLeft(robot)
+
+		robot.say_text("I will drive forward one more time").wait_for_completed()
+		robot.drive_straight(cozmo.util.distance_mm(y_mag), cozmo.util.speed_mmps(150)).wait_for_completed()
+
+		# Turn to face position direction
+		turnRight(robot)
+	
+	else:
+		# Turn in the negative y-direction
+		turnRight(robot)
+
+		# Then move straight
+		robot.say_text("I will drive forward one more time").wait_for_completed()
+		robot.drive_straight(cozmo.util.distance_mm(y_mag), cozmo.util.speed_mmps(150)).wait_for_completed()
+
+		# Turn to face position direction
+		turnLeft(robot)
+
+	robot.play_anim(name=animation).wait_for_completed()
+
+if __name__ == '__main__':
+	cozmo.run_program(cozmo_program, use_viewer=False, force_viewer_on_top=False)
